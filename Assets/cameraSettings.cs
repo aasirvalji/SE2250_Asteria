e353ef348bbb40f68e5cee18b2c1d67554bf3def
av <45238682+avxlji@users.noreply.github.com>
2020-03-05 10:54:32 -0500

    initial commit

---
+++ Assets/cameraSettings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraSetting : MonoBehaviour
+{
+    public GameObject ThirdPersonController;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - ThirdPersonController.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate() // guarenteed to run after all items have been processed and updated
+    {
+        transform.position = ThirdPersonController.transform.position + offset;
+    }
+}
