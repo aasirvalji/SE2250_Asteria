53b275689fc607c86fc251fa932b93bb7ce2b69a
av <45238682+avxlji@users.noreply.github.com>
2020-03-17 19:21:40 -0400

    updated interface

---
+++ Assets/enemyScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class enemyScript : MonoBehaviour
+{
+
+    public Transform Player;
+    float MoveSpeed = 10f;
+    int MaxDist = 10;
+    int MinDist = 5;
+
+    private Rigidbody rb;
+    Vector3 direction;
+    
+
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        direction = Player.position - rb.transform.position;
+
+        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        {
+
+            print(direction);
+            rb.MovePosition(transform.position + (direction * MoveSpeed * Time.deltaTime));
+
+
+            if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+            {
+                //Here Call any function U want Like Shoot at here or something
+            }
+
+        }
+    }
+}
