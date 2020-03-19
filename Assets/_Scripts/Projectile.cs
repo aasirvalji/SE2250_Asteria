fae804b99a7ff0aed5e4aed813b165a3b11fd491
amandaadam <amanda.canada1@gmail.com>
2020-03-19 19:46:25 -0400

    added shooting
    added shooting

---
+++ Assets/_Scripts/Projectile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    public GameObject projectilePrefab; //to get the projectile prefab
+    public float projectileSpeed; //speed
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.Space)) //if user presses space bar, the player should shoot
+        {
+            TempFire();
+
+        }
+    }
+
+    void TempFire() //shooting ability 
+      {
+
+        
+
+          GameObject projGo = Instantiate<GameObject>(projectilePrefab);
+          projGo.transform.position = transform.position;
+          //projGo.transform.rotation= Quaternion.Euler(0.0f, 0.0f, 0.0f);
+
+          projGo.GetComponent<Rigidbody>().velocity = gameObject.transform.position + Vector3.forward * projectileSpeed;
+          //destroy after 3 seconds
+          Destroy(projGo, 3);
+
+
+          }
+
+}
