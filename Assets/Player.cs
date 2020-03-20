95b469eb88ba959e7ce099d1e85b7b0436823fa1
av <45238682+avxlji@users.noreply.github.com>
2020-03-20 10:21:11 -0400

    Completed level 1

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -25,7 +25,27 @@ public class Player : MonoBehaviour
     {
         if (other.tag.Equals("Enemy"))
         {
-            TakeDamage(100);
+            TakeDamage(20);
+        }
+
+        if (other.tag.Equals("Food"))
+        {
+            GetHealth(20);
+            other.gameObject.SetActive(false); //then deactivate the food
+        }
+    }
+
+    void GetHealth(int health)
+    {
+        if (currentHealth == 100)
+        {
+            Debug.Log("Full health");
+        }
+        else
+        {
+            currentHealth += health;
+
+            healthBar.SetHealth(currentHealth);
         }
     }
 
--- Assets/_Scripts/EnemySpawner.cs
+++ Assets/_Scripts/EnemySpawner.cs
@@ -7,10 +7,12 @@ public class EnemySpawner : MonoBehaviour
 
     public GameObject enemyPrefab = null;//public header to get enemeyPrefab
 
+    Vector3 temp = new Vector3(441.0f, 2.0f, 200.0f);
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, transform.position, Quaternion.identity); //enemy appear on the field when game starts
+        Instantiate(enemyPrefab, temp, transform.rotation); //enemy appear on the field when game starts
     }
 
 
