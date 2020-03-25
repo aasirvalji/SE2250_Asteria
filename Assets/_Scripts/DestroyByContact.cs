ab53e7274a9b35654bffc4199d830169b28bd898
amandaadam <amanda.canada1@gmail.com>
2020-03-24 21:49:43 -0400

    Created Level 2
    - waves
    - ship controller
    - shooting
    - enemies spawning
    - boundaries

---
+++ Assets/_Scripts/DestroyByContact.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+	{
+        if(other.tag== "Boundary") //ensures that the monster ignores contact with boundry
+		{
+            return;
+		}
+
+        Destroy(other.gameObject); //destroys the collider (i.e. bolt) 
+        Destroy(gameObject); //destroys itself
+	}
+}
---
+++ Assets/_Scripts/Mover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Mover : MonoBehaviour //script for moving the bolt 
+{
+
+    public float speed;//decided in the inspector 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+
+}
